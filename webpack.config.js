c89664d5726817d1073b2de7ffc8f8815a5488e5
Graham Albachten <albachteng@gmail.com>
2021-05-08 11:09:00 -0500

    add mongoDB URI

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path'); 
 
 module.exports = {
-    entry: './client/index.',
+    entry: './client/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
