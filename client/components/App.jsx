652d031bb7dfb4d404842d34a177ce236455e88b
nonstdout <csalisbury@natilik.com>
2021-05-10 22:28:16 +0100

    added buttons

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
-import Board from './Board.jsx'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+// import Icon from '@material-ui/core/Icon';
+// import SaveIcon from '@material-ui/icons/Save';
+// import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import Quiz from './Quiz.jsx';
+import Board from './Board.jsx'
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 export default function App() {
 
+  const classes = useStyles();
   const [turn,setTurns] = useState(0);
 
   const decrementTurns = () => {
@@ -16,10 +27,14 @@ export default function App() {
       <Board turn={turn} decrementTurns={decrementTurns}/>
       <div style = {{display:"flex",flexDirection:"column"}}>
         <div className="Scoreboard">
-          Turns<span>{turn}</span>
+         <h1>Turns Remaining: <span>{turn}</span></h1>
         </div>
-        <button>Save</button>
-        <button>Load</button>
+        <Button variant="contained" color="secondary">
+        Load
+      </Button>
+        <Button variant="contained" color="primary">
+        Save
+      </Button>
       </div>
       <Quiz turn={turn} setTurns = {setTurns}/>
       {/* <Game /> */}
